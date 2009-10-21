c26bc89f0f5aae3026b72231a61861aa18287ed7
Junichi KAKISAKO <kilo.sw@gmail.com>
2009-10-21 20:14:55 +0900

    (no commit message)

---
+++ src/jp/ac/fit/asura/nao/naimon/ui/ValueTableFrame.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package jp.ac.fit.asura.nao.naimon.ui;
+
+import org.w3c.dom.Document;
+
+/**
+ * @author kilo
+ *
+ */
+public class ValueTableFrame extends NaimonInFrame {
+
+	public ValueTableFrame() {
+		
+	}
+	
+	@Override
+	public String getName() {
+		return "ValueTable";
+	}
+
+	@Override
+	public void update(Document document) {
+		// TODO 自動生成されたメソッド・スタブ
+	}
+
+}
