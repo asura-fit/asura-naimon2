0f5c563b0a19cbf4a87209461324beaee30dc0c6
Junichi KAKISAKO <kilo.sw@gmail.com>
2009-10-31 17:41:06 +0900

    VisionFrameにhough変換のラインを描画するように.

--- src/jp/ac/fit/asura/nao/naimon/ui/VisionFrame.java
+++ src/jp/ac/fit/asura/nao/naimon/ui/VisionFrame.java
@@ -28,32 +28,36 @@ public class VisionFrame extends NaimonInFrame {
 	// images
 	private BufferedImage gcdImage = null;
 	private BufferedImage blobImage = null;
-	
+	private BufferedImage houghImage = null;
+
 	// Panels
 	private ImagePanel imagePanel;
 	private ControlPanel controlPanel;
-	
+
 	public VisionFrame() {
 		init(160, 120);
 		imagePanel = new ImagePanel();
 		controlPanel = new ControlPanel();
-		
+
 		Container cpane = this.getContentPane();
 		BoxLayout layout = new BoxLayout(cpane, BoxLayout.Y_AXIS);
 		cpane.setLayout(layout);
 		cpane.add(imagePanel);
 		cpane.add(controlPanel);
-		
+
 		setMinimumSize(layout.preferredLayoutSize(this.getContentPane()));
 		pack();
 	}
-	
+
 	private void init(int width, int height) {
 		gcdImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		blobImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		blobImage = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_ARGB);
+		houghImage = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_ARGB);
 		setTitle(this.getName() + " " + width + "x" + height);
 	}
-	
+
 	@Override
 	public String getName() {
 		return "Vision";
@@ -61,18 +65,21 @@ public class VisionFrame extends NaimonInFrame {
 
 	@Override
 	public void update(Document document) {
+
 		NodeList gcdNode = document.getElementsByTagName("GCD");
-		Element gcd = (Element)gcdNode.item(0);
+		Element gcd = (Element) gcdNode.item(0);
 		int width = Integer.parseInt(gcd.getAttribute("width"));
 		int height = Integer.parseInt(gcd.getAttribute("height"));
 		int length = Integer.parseInt(gcd.getAttribute("length"));
 		String gdata = gcd.getTextContent();
-		
+		// 使用するBufferedImageを初期化する
+		init(width, height);
+
 		// Base64をでコード後、展開する
-		ByteArrayInputStream bin = new ByteArrayInputStream(Base64.decode(gdata));
+		ByteArrayInputStream bin = new ByteArrayInputStream(Base64
+				.decode(gdata));
 		InflaterInputStream iin = new InflaterInputStream(bin);
 		byte[] gcdPlane = new byte[length];
-		
 		try {
 			int count = 0;
 			while (true) {
@@ -88,28 +95,26 @@ public class VisionFrame extends NaimonInFrame {
 			e.printStackTrace();
 		}
 
-		// 使用するBufferedImageを初期化する
-		init(width, height);
-		int[] pixels = ((DataBufferInt) gcdImage.getRaster().getDataBuffer()).getData();
+		int[] pixels = ((DataBufferInt) gcdImage.getRaster().getDataBuffer())
+				.getData();
 		// gcdPlaneをgcdImageに反映
 		GCD.gcd2rgb(gcdPlane, pixels);
-		
-		
+
 		synchronized (blobImage) {
 			Graphics2D g = blobImage.createGraphics();
 			g.setBackground(new Color(0, 0, 0, 0));
 			g.clearRect(0, 0, width, height);
-			
+
 			NodeList blobs = document.getElementsByTagName("Blobs");
 			for (int i = 0; i < blobs.getLength(); i++) {
-				NodeList blob = (NodeList)blobs.item(i);
-				Element be = (Element)blob;
+				NodeList blob = (NodeList) blobs.item(i);
+				Element be = (Element) blob;
 				int index = Integer.parseInt(be.getAttribute("colorIndex"));
 				g.setColor(getColorWithIndex(index));
 				for (int j = 0; j < blob.getLength(); j++) {
-					Element e = (Element)blob.item(j);
-					int x =  Integer.parseInt(e.getAttribute("xmin"));
-					int y =  Integer.parseInt(e.getAttribute("ymin"));
+					Element e = (Element) blob.item(j);
+					int x = Integer.parseInt(e.getAttribute("xmin"));
+					int y = Integer.parseInt(e.getAttribute("ymin"));
 					int x2 = Integer.parseInt(e.getAttribute("xmax"));
 					int y2 = Integer.parseInt(e.getAttribute("ymax"));
 					g.drawRect(x, y, x2 - x, y2 - y);
@@ -117,14 +122,72 @@ public class VisionFrame extends NaimonInFrame {
 			}
 			g.dispose();
 		}
-		
-		
-		
-		
+
+		Element hough = (Element) document.getElementsByTagName("Hough")
+				.item(0);
+		if (hough != null) {
+			// Houghの表示・非表示を切り替えるためのボタンを有効化
+			controlPanel.houghOnCheckBox.setEnabled(true);
+
+			int hough_width = Integer.parseInt(hough.getAttribute("width"));
+			int hough_height = Integer.parseInt(hough.getAttribute("height"));
+			int hough_length = Integer.parseInt(hough.getAttribute("length"));
+			String hdata = hough.getTextContent();
+
+			// Base64をでコード後、展開する
+			ByteArrayInputStream bin2 = new ByteArrayInputStream(Base64
+					.decode(hdata));
+			InflaterInputStream iin2 = new InflaterInputStream(bin2);
+			byte[] houghPlane = new byte[hough_length];
+			try {
+				int count = 0;
+				while (true) {
+					int ret = iin2.read(houghPlane, count, houghPlane.length
+							- count);
+					if (ret <= 0 || ret == houghPlane.length) {
+						break;
+					} else {
+						count += ret;
+					}
+				}
+				iin2.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			drawHough(houghPlane, hough_width, hough_height);
+		} else {
+			// Houghノードはないのでボタンを無効にする
+			controlPanel.houghOnCheckBox.setEnabled(false);
+		}
+
 		// パネルを再描画
 		imagePanel.repaint();
 	}
-	
+
+	private void drawHough(byte[] houghPlane, int hough_width, int hough_height) {
+		synchronized (houghImage) {
+			Graphics2D g = houghImage.createGraphics();
+			g.setColor(Color.GRAY.darker());
+			int w = 360 / hough_height;
+			for (int y = 0; y < hough_height; y++) {
+				for (int x = 0; x < hough_width; x++) {
+					if ((houghPlane[y * hough_width + x] & 0xFF) > 128) {
+						double th = Math.toRadians(y * w);
+						if (y * w % 90 == 0)
+							continue;
+						int rho = x * 2;
+						int x0 = 0, y0, x1, y1 = 0;
+						y0 = (int) (rho / Math.sin(th));
+						x1 = (int) (rho / Math.cos(th));
+						g.drawLine(x0, y0, x1, y1);
+					}
+				}
+			}
+			g.dispose();
+		}
+	}
+
 	private Color getColorWithIndex(int index) {
 		Color color = null;
 		switch (index) {
@@ -141,87 +204,109 @@ public class VisionFrame extends NaimonInFrame {
 		}
 		return color;
 	}
-	
+
 	class ImagePanel extends JPanel {
 
 		public ImagePanel() {
 			setMinimumSize(new Dimension(160, 120));
 			setPreferredSize(new Dimension(160, 120));
 		}
-		
+
 		@Override
 		protected void paintComponent(Graphics g) {
 			g.setColor(Color.GRAY);
 			g.fillRect(0, 0, getWidth(), getHeight());
-			
+
 			int drawWidth = gcdImage.getWidth();
 			int drawHeight = gcdImage.getHeight();
 			int x = (getWidth() - drawWidth) / 2;
 			int y = (getHeight() - drawHeight) / 2;
 			if (controlPanel.isAutoScale) {
 				if (getWidth() > getHeight()) {
-					double n = (double)gcdImage.getWidth() / gcdImage.getHeight();
-					drawHeight = (int)(getHeight() * 0.9); // 90%
-					drawWidth = (int)(drawHeight * n);
+					double n = (double) gcdImage.getWidth()
+							/ gcdImage.getHeight();
+					drawHeight = (int) (getHeight() * 0.9); // 90%
+					drawWidth = (int) (drawHeight * n);
 					x = (getWidth() - drawWidth) / 2;
 					y = (getHeight() - drawHeight) / 2;
 				} else {
-					double n = (double)gcdImage.getHeight() / gcdImage.getWidth();
-					drawWidth = (int)(getWidth() * 0.9); // 90%
-					drawHeight = (int)(drawWidth * n);
+					double n = (double) gcdImage.getHeight()
+							/ gcdImage.getWidth();
+					drawWidth = (int) (getWidth() * 0.9); // 90%
+					drawHeight = (int) (drawWidth * n);
 					x = (getWidth() - drawWidth) / 2;
 					y = (getHeight() - drawHeight) / 2;
 				}
 			}
-			
-			g.drawImage(gcdImage, x, y, drawWidth, drawHeight, Color.BLACK, null);
-			
+
+			// CGDImageを描画
+			g.drawImage(gcdImage, x, y, drawWidth, drawHeight, Color.BLACK,
+					null);
+			// Houghを描画
+			if (controlPanel.isHoughOn) {
+				synchronized (houghImage) {
+					g.drawImage(houghImage, x, y, drawWidth, drawHeight, null);
+				}
+			}
+			// BlobImageを描画
 			if (controlPanel.isBlobOn) {
 				synchronized (blobImage) {
 					g.drawImage(blobImage, x, y, drawWidth, drawHeight, null);
 				}
 			}
 		}
-		
+
 	}
-	
+
 	class ControlPanel extends JPanel {
-		
-		protected boolean isBlobOn = true;
+
 		protected boolean isAutoScale = true;
-		
+		protected boolean isBlobOn = true;
+		protected boolean isHoughOn = true;
+
+		protected JCheckBox houghOnCheckBox;
+
 		public ControlPanel() {
 			BoxLayout layout = new BoxLayout(this, BoxLayout.X_AXIS);
 			setLayout(layout);
-			
+
 			JCheckBox blobOnCheckBox = new JCheckBox("Blob表示");
 			blobOnCheckBox.setSelected(isBlobOn);
-			blobOnCheckBox.addActionListener(new ActionListener(){
+			blobOnCheckBox.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					isBlobOn = !isBlobOn;	
+					isBlobOn = !isBlobOn;
+				}
+			});
+			houghOnCheckBox = new JCheckBox("Hough表示");
+			houghOnCheckBox.setSelected(isHoughOn);
+			houghOnCheckBox.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					isHoughOn = !isHoughOn;
 				}
 			});
 			JCheckBox autoScaleCheckBox = new JCheckBox("自動スケール");
 			autoScaleCheckBox.setSelected(isAutoScale);
-			autoScaleCheckBox.addActionListener(new ActionListener(){
+			autoScaleCheckBox.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					isAutoScale = !isAutoScale;
 					imagePanel.repaint();
 				}
 			});
-			
+
 			add(blobOnCheckBox);
+			add(houghOnCheckBox);
 			add(autoScaleCheckBox);
-			
+
 			setMaximumSize(layout.preferredLayoutSize(this));
 		}
-		
+
 	}
-	
+
 	static class GCD {
-		
+
 		public static final byte cORANGE = 0;
 		public static final byte cCYAN = 1;
 		public static final byte cGREEN = 2;
@@ -233,7 +318,7 @@ public class VisionFrame extends NaimonInFrame {
 		// public static final byte cFGREEN = 7;
 		public static final byte cBLACK = 9;
 		public static final int COLOR_NUM = 10;
-		
+
 		public static void gcd2rgb(byte[] gcdPlane, int[] rgbPlane) {
 			for (int i = 0; i < gcdPlane.length; i++) {
 				switch (gcdPlane[i]) {
@@ -266,7 +351,7 @@ public class VisionFrame extends NaimonInFrame {
 				}
 			}
 		}
-		
+
 	}
 
 }
